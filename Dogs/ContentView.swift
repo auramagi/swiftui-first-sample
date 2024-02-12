8199458b3f26a00ef367fc5be417ce1bfde728c8
Mikhail Apurin <m_apurin@yumemi.co.jp>
2024-02-12 13:55:32 +0900

    Initial Commit

---
+++ Dogs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dogs
+//
+//  Created by Mikhail Apurin on 2024-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Mikhail Apurin on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
